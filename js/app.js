31c99c440e4ca235cc162b3764a8e08fa21f6cbf
swzhou <swing.zhou@hotmail.com>
2014-06-08 20:45:21 +0800

    [sw] re-initialize

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 (function(){
+    if ("ontouchstart" in document.documentElement) {
+        document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
+    }
     impress().init();
 })();
